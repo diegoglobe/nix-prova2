5d3e83e22ce7d56de4d9c07582c90a703c3daefb
diegoglobe <larosadiego15@gmail.com>
2025-12-08 13:26:35 +0100

    gin

--- nuovo-nix/modules/tomcat.nix
+++ nuovo-nix/modules/tomcat.nix
@@ -1,20 +1,68 @@
-# modules/tomcat.nix
-{ config, pkgs, ... }:
+# PRIMA: rimuovi il file danneggiato
+rm modules/tomcat.nix
 
+# POI: crea il file CORRETTO
+cat > modules/tomcat.nix << 'EOF'
+{ config, pkgs, ... }:
 {
   services.tomcat = {
     enable = true;
     package = pkgs.tomcat10;
     
-    # Non serve più specificare user/group, NixOS li gestisce
-    
-    # Java options
-    jvmOpts = [
+    # ✅ CORRETTO per nixos-23.11: javaOpts (non jvmOpts)
+    javaOpts = [
       "-Xms512m"
       "-Xmx1024m"
       "-Djava.awt.headless=true"
       "-Dfile.encoding=UTF-8"
-      "-Djava.security.egd=file:/dev/./urandom"
     ];
+    
+    # Configurazione base
+    user = "tomcat";
+    group = "tomcat";
+    baseDir = "/var/lib/tomcat";
   };
+  
+  # Crea utente e gruppo tomcat
+  users.users.tomcat = {
+    isSystemUser = true;
+    group = "tomcat";
+    home = "/var/lib/tomcat";
+    createHome = true;
+  };
+  users.groups.tomcat = {};
+}
+EOF# PRIMA: rimuovi il file danneggiato
+rm modules/tomcat.nix
+
+# POI: crea il file CORRETTO
+cat > modules/tomcat.nix << 'EOF'
+{ config, pkgs, ... }:
+{
+  services.tomcat = {
+    enable = true;
+    package = pkgs.tomcat10;
+    
+    # ✅ CORRETTO per nixos-23.11: javaOpts (non jvmOpts)
+    javaOpts = [
+      "-Xms512m"
+      "-Xmx1024m"
+      "-Djava.awt.headless=true"
+      "-Dfile.encoding=UTF-8"
+    ];
+    
+    # Configurazione base
+    user = "tomcat";
+    group = "tomcat";
+    baseDir = "/var/lib/tomcat";
+  };
+  
+  # Crea utente e gruppo tomcat
+  users.users.tomcat = {
+    isSystemUser = true;
+    group = "tomcat";
+    home = "/var/lib/tomcat";
+    createHome = true;
+  };
+  users.groups.tomcat = {};
 }
